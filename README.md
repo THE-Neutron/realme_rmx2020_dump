65c18bf237f25505627544a3c7fb35b6ca2b4433
root <root@fv-az163-731.kzyshwy3mise5klwsllqwzgzyb.cx.internal.cloudapp.net>
2022-06-27 18:16:02 +0000

    Add extras for full_oppo6769-user 10 QP1A.190711.020 68b77aba7cb33275 release-keys
    Signed-off-by: root <root@fv-az163-731.kzyshwy3mise5klwsllqwzgzyb.cx.internal.cloudapp.net>

---
+++ README.md
@@ -0,0 +1,21 @@
+## full_oppo6769-user 10 QP1A.190711.020 68b77aba7cb33275 release-keys
+- Manufacturer: realme
+- Platform: mt6768
+- Codename: RMX2020
+- Brand: realme
+- Flavor: full_oppo6769-user
+- Release Version: 10
+- Id: QP1A.190711.020
+- Incremental: 0560_202106161737
+- Tags: release-keys
+- CPU Abilist: arm64-v8a,armeabi-v7a,armeabi
+- A/B Device: false
+- Locale: en-US
+- Screen Density: undefined
+- Fingerprint: 
+- OTA version: RMX2020EU_11.A.56_0560_202106161737
+- Branch: RMX2020EU_11.A.56_0560_202106161737
+- Repo: realme_rmx2020_dump
+
+
+>Dumped by [Phoenix Firmware Dumper](https://github.com/DroidDumps/phoenix_firmware_dumper)
---
+++ join_split_files.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+cat system/system/preload/HeyFun/HeyFun.apk.* 2>/dev/null >> system/system/preload/HeyFun/HeyFun.apk
+rm -f system/system/preload/HeyFun/HeyFun.apk.* 2>/dev/null
+cat system/system/apex/com.android.runtime.release.apex.* 2>/dev/null >> system/system/apex/com.android.runtime.release.apex
+rm -f system/system/apex/com.android.runtime.release.apex.* 2>/dev/null
+cat system/system/priv-app/SettingsEU/SettingsEU.apk.* 2>/dev/null >> system/system/priv-app/SettingsEU/SettingsEU.apk
+rm -f system/system/priv-app/SettingsEU/SettingsEU.apk.* 2>/dev/null
+cat system/system/priv-app/OppoGallery2EU/OppoGallery2EU.apk.* 2>/dev/null >> system/system/priv-app/OppoGallery2EU/OppoGallery2EU.apk
+rm -f system/system/priv-app/OppoGallery2EU/OppoGallery2EU.apk.* 2>/dev/null
+cat system/system/app/OppoCamera/OppoCamera.apk.* 2>/dev/null >> system/system/app/OppoCamera/OppoCamera.apk
+rm -f system/system/app/OppoCamera/OppoCamera.apk.* 2>/dev/null
+cat system/system/app/GoogleLatinInput/GoogleLatinInput.apk.* 2>/dev/null >> system/system/app/GoogleLatinInput/GoogleLatinInput.apk
+rm -f system/system/app/GoogleLatinInput/GoogleLatinInput.apk.* 2>/dev/null
+cat system/system/product/priv-app/GmsCore/GmsCore.apk.* 2>/dev/null >> system/system/product/priv-app/GmsCore/GmsCore.apk
+rm -f system/system/product/priv-app/GmsCore/GmsCore.apk.* 2>/dev/null
+cat system/system/product/priv-app/Velvet/Velvet.apk.* 2>/dev/null >> system/system/product/priv-app/Velvet/Velvet.apk
+rm -f system/system/product/priv-app/Velvet/Velvet.apk.* 2>/dev/null
+cat system/system/product/app/YouTube/YouTube.apk.* 2>/dev/null >> system/system/product/app/YouTube/YouTube.apk
+rm -f system/system/product/app/YouTube/YouTube.apk.* 2>/dev/null
+cat system/system/product/app/WebViewGoogle/WebViewGoogle.apk.* 2>/dev/null >> system/system/product/app/WebViewGoogle/WebViewGoogle.apk
+rm -f system/system/product/app/WebViewGoogle/WebViewGoogle.apk.* 2>/dev/null
